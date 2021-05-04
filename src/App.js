11788b6a7f2822aea46105a74cbd6bc2ddb52ce3
legloc <aliiev.akhtem@gmail.com>
2021-05-04 17:53:06 +0300

    Added images to cards

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import Card from './components/Card'
+
+import JakeGyllenhaalImage from './images/JakeGyllenhaal.jpg'
+import JoaquinPhoenixImage from './images/JoaquinPhoenix.jpg'
+import MadsMikkelsenImage from './images/MadsMikkelsen.jpg'
+
 import './App.sass';
 
 const profiles = [{
   id: 1,
-  name: 'Jake Gyllenhaal'
+  name: 'Jake Gyllenhaal',
+  image: JakeGyllenhaalImage
 }, {
   id: 2,
-  name: 'Joaquin Phoenix'
+  name: 'Joaquin Phoenix',
+  image: JoaquinPhoenixImage
 }, {
   id: 3,
-  name: 'Mads Mikkelsen'
+  name: 'Mads Mikkelsen',
+  image: MadsMikkelsenImage
 }, ]
 
 const App = () => (
   <div className="wrapper">
     { profiles.map(profile => (
-      <Card key={ profile.id } name={ profile.name } />
+      <Card key={ profile.id } name={ profile.name } image={ profile.image } />
     )) }
   </div>
 )
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,26 @@
-const Card = ({ name }) => (
+const Card = ({ name, image }) => (
   <div className="profile-card">
+    <img className="card-img" src={ image } alt={ name } />
     <div className="card-body">
-      <h2 className="card-title">{ name }</h2>
-      <div className="card-links">
-        <a className="link-title" href="/">
-          <ion-icon name="logo-facebook" />
-        </a>
-        <a className="link-title" href="/">
-          <ion-icon name="logo-twitter" />
-        </a>
-        <a className="link-title" href="/">
-          <ion-icon name="logo-instagram" />
-        </a>
-        <a className="link-title" href="/">
-          <ion-icon name="logo-github" />
-        </a>
-        <a className="link-title" href="/">
-          <ion-icon name="logo-dribbble" />
-        </a>
+      <div className="card-content">
+        <h2 className="card-title">{ name }</h2>
+        <div className="card-links">
+          <a className="link-title" href="/">
+            <ion-icon name="logo-facebook" />
+          </a>
+          <a className="link-title" href="/">
+            <ion-icon name="logo-twitter" />
+          </a>
+          <a className="link-title" href="/">
+            <ion-icon name="logo-instagram" />
+          </a>
+          <a className="link-title" href="/">
+            <ion-icon name="logo-github" />
+          </a>
+          <a className="link-title" href="/">
+            <ion-icon name="logo-dribbble" />
+          </a>
+        </div>
       </div>
     </div>
   </div>
