f7d4f97d90b824fbe69cc434c79bd6651ce0e376
legloc <aliiev.akhtem@gmail.com>
2021-05-04 15:52:24 +0300

    Created Card template

--- src/App.js
+++ src/App.js
@@ -2,9 +2,72 @@ import './App.sass';
 
 const App = () => (
   <div className="wrapper">
-    <div className="profile-card"></div>
-    <div className="profile-card"></div>
-    <div className="profile-card"></div>
+    <div className="profile-card">
+      <div className="card-body">
+        <h2 className="card-title">Jake Gyllenhaal</h2>
+        <div className="card-links">
+          <a className="link-title" href="/">
+            <ion-icon name="logo-facebook" />
+          </a>
+          <a className="link-title" href="/">
+            <ion-icon name="logo-twitter" />
+          </a>
+          <a className="link-title" href="/">
+            <ion-icon name="logo-instagram" />
+          </a>
+          <a className="link-title" href="/">
+            <ion-icon name="logo-github" />
+          </a>
+          <a className="link-title" href="/">
+            <ion-icon name="logo-dribbble" />
+          </a>
+        </div>
+      </div>
+    </div>
+    <div className="profile-card">
+      <div className="card-body">
+        <h2 className="card-title">Joaquin Phoenix</h2>
+        <div className="card-links">
+          <a className="link-title" href="/">
+            <ion-icon name="logo-facebook" />
+          </a>
+          <a className="link-title" href="/">
+            <ion-icon name="logo-twitter" />
+          </a>
+          <a className="link-title" href="/">
+            <ion-icon name="logo-instagram" />
+          </a>
+          <a className="link-title" href="/">
+            <ion-icon name="logo-github" />
+          </a>
+          <a className="link-title" href="/">
+            <ion-icon name="logo-dribbble" />
+          </a>
+        </div>
+      </div>
+    </div>
+    <div className="profile-card">
+      <div className="card-body">
+        <h2 className="card-title">Mads Mikkelsen</h2>
+        <div className="card-links">
+          <a className="link-title" href="/">
+            <ion-icon name="logo-facebook" />
+          </a>
+          <a className="link-title" href="/">
+            <ion-icon name="logo-twitter" />
+          </a>
+          <a className="link-title" href="/">
+            <ion-icon name="logo-instagram" />
+          </a>
+          <a className="link-title" href="/">
+            <ion-icon name="logo-github" />
+          </a>
+          <a className="link-title" href="/">
+            <ion-icon name="logo-dribbble" />
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
 )
 
